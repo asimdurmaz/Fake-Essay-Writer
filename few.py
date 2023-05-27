84b5c9cc92ded712ebb4628edace3f4c421e9013
asimdurmaz <87034877+asimdurmaz@users.noreply.github.com>
2023-05-28 00:35:17 +0300

    Create README.md

---
+++ few.py
@@ -0,0 +1,24 @@
+# Import the required libraries
+
+import pyautogui
+import random
+import time
+
+# Function to generate a random float each time and type the given text.
+
+
+def type_with_random_interval(text):
+    time.sleep(3)
+    for char in text:
+        pyautogui.press(char)
+        interval = random.uniform(0.02, 0.45)
+        pyautogui.PAUSE = interval
+
+
+# Get the text from the user
+
+user_input = input("Enter the text to be typed: ")
+
+# Call the function
+
+type_with_random_interval(user_input)
